3534975b99b079d82f0285b2289085cc3a46000c
Mr-Bowtie <mr.bowtie989@gmail.com>
2021-07-21 04:31:58 -0500

    board display & player turn done. working on game end detection

--- Ruby_programming/tic-tac-toe/tic-tac-toe.rb
+++ Ruby_programming/tic-tac-toe/tic-tac-toe.rb
@@ -1,9 +1,16 @@
+require "pry"
+
 class Board
-  attr_reader :board_positions, :size
+  WINNING_POSITIONS = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 9], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+  attr_reader :board_positions, :size, :valid_positions
 
   def initialize(size = 3)
     @size = size
-    @board_positions = (1..(size ** 2)).to_a
+    @board_positions = (1..(size ** 2)).to_a.map { |el| el }
+  end
+
+  def calc_valid_positions
+    self.board_positions.select { |el| (1..(size ** 2)).include?(el) }
   end
 
   def display_separator
@@ -14,7 +21,7 @@ class Board
     puts "| #{s1} | #{s2} | #{s3} |"
   end
 
-  def display_board
+  def display_board #todo: make the position numbers a different color / opacity than the players symbol
     puts ""
     display_separator()
     display_boxes(@board_positions[0], @board_positions[1], @board_positions[2])
@@ -25,19 +32,43 @@ class Board
     display_separator()
     puts ""
   end
+
+  def game_over?
+    return true if self.calc_valid_positions == []
+    positions = self.board_positions
+    p positions
+    WINNING_POSITIONS.each do |group|
+      if group.all? { |i| positions[i] == "X" } || group.all? { |i| positions[i] == "O" }
+        # binding.pry
+        return true
+      end
+    end
+    false
+  end
 end
 
 class Player
   attr_reader :name
+  attr_accessor :board, :symbol, :first_move
 
-  def initialize(name)
+  def initialize(name, board)
     @name = name
     @score = 0
     @first_move = false
+    @board = board
+    @symbol = "O"
   end
 
   def go_first
     self.first_move = true
+    self.symbol = "X"
+  end
+
+  def take_turn
+    puts "Choose an open position on the board #{self.board.calc_valid_positions}" #todo: validate input and update available position
+    choice = gets.chomp.to_i
+    position = self.board.board_positions.index(choice)
+    self.board.board_positions[position] = self.symbol
   end
 end
 
@@ -49,6 +80,15 @@ class Computer < Player
   end
 end
 
+def play_round(player, board)
+  loop do
+    board.display_board
+    player.take_turn
+    # binding.pry
+    break if board.game_over?
+  end
+end
+
 board = Board.new
-board.display_board
-puts board.size
+ian = Player.new("Ian", board)
+play_round(ian, board)
